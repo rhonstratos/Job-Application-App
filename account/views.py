2431737f4e604b5cf4191821509b55f148508991
rhonstratos <kizunaai20@gmail.com>
2023-01-02 17:02:45 +0800

    prepare employer edit profile

--- account/views.py
+++ account/views.py
@@ -2,15 +2,14 @@ from django.contrib import auth
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
-from django.shortcuts import render, redirect , get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse, reverse_lazy
 
 from account.forms import *
-from jobapp.permission import user_is_employee 
+from jobapp.permission import user_is_employee
 
 
 def get_success_url(request):
-
     """
     Handle Success Url After LogIN
 
@@ -21,83 +20,102 @@ def get_success_url(request):
         return reverse('jobapp:home')
 
 
-
 def employee_registration(request):
-
     """
     Handle Employee Registration
 
     """
-    form = EmployeeRegistrationForm(request.POST or None, request.FILES or None)
+    form = EmployeeRegistrationForm(
+        request.POST or None, request.FILES or None)
     if form.is_valid():
         form = form.save()
         return redirect('account:login')
-    context={
-        
-            'form':form
-        }
+    context = {
 
-    return render(request,'account/employee-registration.html',context)
+        'form': form
+    }
+
+    return render(request, 'account/employee-registration.html', context)
 
 
 def employer_registration(request):
-
     """
     Handle Employee Registration 
 
     """
 
-    form = EmployerRegistrationForm(request.POST or None, request.FILES or None)
+    form = EmployerRegistrationForm(
+        request.POST or None, request.FILES or None)
     if form.is_valid():
         form = form.save()
         return redirect('account:login')
-    context={
-        
-            'form':form
-        }
+    context = {
 
-    return render(request,'account/employer-registration.html',context)
+        'form': form
+    }
+
+    return render(request, 'account/employer-registration.html', context)
+
+
+def employer_edit_profile(request, id=id):
+    """
+    Handle Employer Profile Update Functionality
+
+    """
+
+    user = get_object_or_404(User, id=id)
+    form = EmployeeProfileEditForm(
+        request.POST or None, request.FILES or None, instance=user)
+    if form.is_valid():
+        form = form.save()
+        messages.success(request, 'Your Profile Was Successfully Updated!')
+        return redirect(reverse("account:employer-profile", kwargs={
+            'id': form.id
+        }))
+    context = {
+        'user': user,
+        'form': form
+    }
+
+    return render(request, 'account/employer-edit-profile.html', context)
 
 
 @login_required(login_url=reverse_lazy('accounts:login'))
 @user_is_employee
 def employee_edit_profile(request, id=id):
-
     """
     Handle Employee Profile Update Functionality
 
     """
 
     user = get_object_or_404(User, id=id)
-    form = EmployeeProfileEditForm(request.POST or None, request.FILES or None, instance=user)
+    form = EmployeeProfileEditForm(
+        request.POST or None, request.FILES or None, instance=user)
     if form.is_valid():
         form = form.save()
         messages.success(request, 'Your Profile Was Successfully Updated!')
         return redirect(reverse("account:edit-profile", kwargs={
-                                    'id': form.id
-                                    }))
-    context={
-			'user':user,
-            'form':form
-        }
+            'id': form.id
+        }))
+    context = {
+        'user': user,
+        'form': form
+    }
 
-    return render(request,'account/employee-edit-profile.html',context)
-
+    return render(request, 'account/employee-edit-profile.html', context)
 
 
 def user_logIn(request):
-
     """
     Provides users to logIn
 
     """
 
     form = UserLoginForm(request.POST or None)
-    
 
     if request.user.is_authenticated:
         return redirect('/')
-    
+
     else:
         if request.method == 'POST':
             if form.is_valid():
@@ -107,7 +125,7 @@ def user_logIn(request):
         'form': form,
     }
 
-    return render(request,'account/login.html',context)
+    return render(request, 'account/login.html', context)
 
 
 def user_logOut(request):
